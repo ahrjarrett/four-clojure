ffc1d2769dff963a074a08ac4f381d3f27dd262d
Andrew Jarrett <ahrjarrett@gmail.com>
2017-05-16 00:43:26 -0600

    finished intro to fns

--- src/four_clojure/core.clj
+++ src/four_clojure/core.clj
@@ -40,12 +40,36 @@
 
 ;; Intro to Maps
 ;; (= __ ((hash-map :a 10, :b 20, :c 30) :b))
-(= __ ((hash-map :a 10, :b 20, :c 30) :b))
+;; (= __ (:b {:a 10, :b 20, :c 30}))
+(= 20 ((hash-map :a 10, :b 20, :c 30) :b))
+(= 20 (:b {:a 10, :b 20, :c 30}))
 
+;; Maps: conj
+;; (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3]))
+(= {:a 1, :b 2, :c 3} (conj {:a 1} [:b 2] [:c 3]))
 
+;; Intro to Sequences
+;; (= __ (first '(3 2 1)))
+;; (= __ (second [2 3 4]))
+;; (= __ (last (list 1 2 3)))
+(= 3 (first '(3 2 1)))
+(= 3 (second [2 3 4]))
+(= 3 (last (list 1 2 3)))
 
+;; Sequences: Rest
+;; (= __ (rest [10 20 30 40]))
+(= [20 30 40] (rest [10 20 30 40]))
 
-
+;; Intro to Functions
+;; (= __ ((fn add-five [x] (+ x 5)) 3))
+;; (= __ ((fn add-five [x] (+ x 5)) 3))
+;; (= __ ((fn [x] (+ x 5)) 3))
+;; (= __ (#(+ % 5) 3))
+;; (= __ ((partial + 5) 3))
+(= 8 ((fn add-five [x] (+ x 5)) 3))
+(= 8 ((fn [x] (+ x 5)) 3))
+(= 8 (#(+ % 5) 3))
+(= 8 ((partial + 5) 3))
 
 
 
